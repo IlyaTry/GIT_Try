46daa77ddec845e8ab4d3fb4537f8f3b53a3a01f
Illia Dudnyk <illia.dudnyk@raiffeisen.ua>
2023-12-28 16:55:11 +0200

    second branch with Gui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("something in new branch (GUI)");
+        System.out.println("something in new branch (GUI) second");
     }
 }
